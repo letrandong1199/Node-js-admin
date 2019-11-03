d441934968bea11925090d80c71272b0c6df774d
letrandong1199 <49052236+letrandong1199@users.noreply.github.com>
2019-11-03 14:21:06 +0700

    Routing all html

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,32 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+router.get('/index.html', function(req, res, next) {
+  res.render('index');
+});
+router.get('/inbox.html', function(req, res, next) {
+  res.render('inbox');
+});
+router.get('/compose.html', function(req, res, next) {
+  res.render('compose');
+});
+router.get('/validation.html', function(req, res, next) {
+  res.render('validation');
+});
+router.get('/forms.html', function(req, res, next) {
+  res.render('forms');
+});
+router.get('/graphs.html', function(req, res, next) {
+  res.render('graphs');
+});
+router.get('/basic_tables.html', function(req, res, next) {
+  res.render('basic_tables');
+});
+router.get('/login.html', function(req, res, next) {
+  res.render('login');
+});
+
 
 module.exports = router;
